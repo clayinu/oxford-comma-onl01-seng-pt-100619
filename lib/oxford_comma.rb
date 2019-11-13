3819acea67d445219c1cdc0ba3e579888661afc9
Hoko Le <hokole527@gmail.com>
2019-11-13 21:34:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
   array.join
-  array.join(" and ")
-  array.to_sentence
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
